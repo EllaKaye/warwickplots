bcea7007206cbbb94853ce15fe82c09c7cdfa6a2
EllaKaye <ella.kaye@gmail.com>
2023-02-28 12:04:34 +0000

    Add documentation

---
+++ R/palettes.R
@@ -0,0 +1,31 @@
+#' University of Warwick palettes
+#'
+#' Palettes consistent with The University of Warwick's ID7 branding.
+#'
+#' @format ## `warwick_palettes`
+#' An object of class `palettes_palette` with `r length(warwick_palettes)`
+#' colour palettes. Use `names(warwick_palettes)` to return all palette names.
+#' @source <https://github.com/Warwick-Stats-Resources/warwickplots>
+#' @author [Ella Kaye](https://github.com/Ella Kaye)
+#' @seealso [pal_palette()], [pal_colour()]
+#' @examples
+#' # Get all palettes by name.
+#' names(warwick_palettes)
+#'
+#' # Plot all palettes.
+#' plot(warwick_palettes)
+"warwick_palettes"
+
+#' @rdname warwick_palettes
+#' @format ## `warwick_palettes_discrete`
+#' An object of class `palettes_palette` with
+#' `r length(warwick_palettes_discrete)` discrete colour palettes.
+#'  Use `names(warwick_palettes_discrete)` to return all palette names.
+"warwick_palettes_discrete"
+
+#' @rdname warwick_palettes
+#' @format ## `warwick_palettes_sequential`
+#' An object of class `palettes_palette` with
+#' `r length(warwick_palettes_sequential)` sequential colour palettes.
+#'  Use `names(warwick_palettes_sequential)` to return all palette names.
+"warwick_palettes_sequential"
