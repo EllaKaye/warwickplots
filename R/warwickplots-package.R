6b5b4e98159b0c9b01dbe03f54b2fe42526f1645
EllaKaye <ella.kaye@gmail.com>
2023-02-28 12:04:19 +0000

    Add {palettes} dependency

---
+++ R/warwickplots-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+#' Internal palettes methods
+#'
+#' @import palettes
+#' @keywords internal
+#' @name warwick-palettes
+NULL
