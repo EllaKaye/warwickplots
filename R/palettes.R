568b23e4d550a788407ecda446680574a3d55b5b
EllaKaye <ella.kaye@gmail.com>
2024-05-16 15:01:50 +0100

    Fix documentation error

--- R/palettes.R
+++ R/palettes.R
@@ -1,6 +1,6 @@
 #' University of Warwick palettes
 #'
-#' Palettes consistent with The University of Warwick's ID7 branding.
+#' Palettes consistent with The University of Warwick's brand.
 #'
 #' @format ## `warwick_palettes`
 #' An object of class `palettes_palette` with `r length(warwick_palettes)`
@@ -13,7 +13,7 @@
 #' names(warwick_palettes)
 #'
 #' # Plot all palettes.
-#' plot(warwick_palettes)
+#' plot(warwick_palettes$primary)
 "warwick_palettes"
 
 #' @rdname warwick_palettes
@@ -28,4 +28,11 @@
 #' An object of class `palettes_palette` with
 #' `r length(warwick_palettes_sequential)` sequential colour palettes.
 #'  Use `names(warwick_palettes_sequential)` to return all palette names.
+"warwick_palettes_sequential"
+
+#' @rdname warwick_palettes
+#' @format ## `warwick_palettes_divergent`
+#' An object of class `palettes_palette` with
+#' `r length(warwick_palettes_divergent)` sequential colour palettes.
+#'  Use `names(warwick_palettes_divergent)` to return all palette names.
 "warwick_palettes_sequential"
