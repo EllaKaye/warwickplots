6f8f606836bae5aee4d6cb053419714e667dcce7
EllaKaye <ella.kaye@gmail.com>
2024-05-24 11:41:28 +0100

    theme_warwick tweaks

---
+++ R/theme_warwick.R
@@ -0,0 +1,43 @@
+#showtext::font_add_google(name = "Lato", family = "Lato")
+
+#' ggplot2 theme
+#'
+#' @param base_size numeric, base size for font
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(cty, hwy)) +
+#'    ggplot2::geom_point() +
+#'    theme_warwick()
+#' }
+
+theme_warwick <- function(base_size = 11) {
+
+  dark_text <- "#383838"
+  mid_text <-  "#878787"
+  light_text <- "#AFAFAF"
+  pale_text <- "#EBEBEB"
+
+  ggplot2::theme_minimal(base_size = base_size) +
+    ggplot2::theme(text = ggplot2::element_text(colour = dark_text,
+                              lineheight = 1.1,
+                              family = "Lato"
+
+    ),
+    plot.title = ggtext::element_textbox_simple(colour = dark_text,
+                                        size = ggplot2::rel(1.4),
+                                        margin = ggplot2::margin(12, 0, 6, 0)),
+    plot.title.position = "plot",
+    plot.subtitle = ggtext::element_textbox_simple(size = ggplot2::rel(1.1), margin = ggplot2::margin(4, 0, 4, 0)),
+    axis.text.y = ggplot2::element_text(colour = mid_text),
+    axis.title.y = ggplot2::element_text(size = base_size, margin = ggplot2::margin(0, 4, 0, 0)),
+    axis.text.x = ggplot2::element_text(colour = mid_text, size = base_size),
+    axis.title.x = ggplot2::element_text(size = base_size, margin = ggplot2::margin(0, 4, 0, 0)),
+    legend.position = "top",
+    legend.justification = 1,
+    panel.grid = ggplot2::element_line(colour = pale_text),
+    plot.caption = ggplot2::element_text(size = ggplot2::rel(0.8), margin = ggplot2::margin(8, 0, 0, 0)),
+    plot.margin = ggplot2::margin(0.25, 0.25, 0.25, 0.25,"cm"))
+}
